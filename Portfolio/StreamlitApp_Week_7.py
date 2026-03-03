b09b7756f94e156e00e7ff078a370a6a90ce8866
kogamer4now <kogamer4now@gmail.com>
2026-03-03 17:38:52 -0600

    Update StreamlitApp_Week_7.py

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -118,7 +118,7 @@ def display_explanation(input_df, session, aws_bucket):
     preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-2])
     input_df_transformed = preprocessing_pipeline.transform(input_df)
     feature_names = best_pipeline[1:4].get_feature_names_out()
-    X_test_transformed = pd.DataFrame(X_test_transformed, columns=feature_names)
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
     shap_values = explainer(input_df_transformed)
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
@@ -159,6 +159,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
