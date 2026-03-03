7fefaa76eb57f4282970a035b1342748ab8f7cc6
kogamer4now <kogamer4now@gmail.com>
2026-03-03 17:26:21 -0600

    Modify model input keys and parameters
    Updated model input keys and adjusted input parameters for prediction.

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["AAPL", "MPWR"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AAPL", "MPWR"]]
+        "keys": ["AME", "AAPL"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AME", "AAPL"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -159,6 +159,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
