5ef068fcbf9cadf4680465192032779aa48d2670
kogamer4now <kogamer4now@gmail.com>
2026-04-02 18:09:33 -0500

    Restore imports and session management for Streamlit app

--- Portfolio/StreamlitApp_Week_5_Part1.py
+++ Portfolio/StreamlitApp_Week_5_Part1.py
@@ -97,7 +97,7 @@ def call_model_api(input_df):
     predictor = Predictor(
         endpoint_name=MODEL_INFO["endpoint"],
         sagemaker_session=sm_session,
-        serializer=NumpySerializer(),
+        serializer=JSONSerializer(),
         deserializer=NumpyDeserializer() 
     )
 
