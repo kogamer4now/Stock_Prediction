14561539039de0ca52222c6cbd882050cf610df0
kogamer4now <kogamer4now@gmail.com>
2026-04-14 18:00:30 -0500

    Refactor imports and enhance session management

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -114,7 +114,8 @@ def call_model_api(input_df):
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
         mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
-        return mapping.get(pred_val), 200
+        return round(float(pred_val), 4), 200
+        #return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
 
